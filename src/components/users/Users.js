082da4fd3691b7eedea14c28bb2bc8e52152f14f
Ian VanDooren <63620074+Ian-VanDooren@users.noreply.github.com>
2020-12-07 16:15:28 -0800

    Add files via upload

---
+++ src/components/users/Users.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+    state = {
+        users: [
+            
+        ]
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Users
