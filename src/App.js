d7a2ace6f6280b0c8a5e50aaed75dbc2d5deea11
Ian VanDooren <63620074+Ian-VanDooren@users.noreply.github.com>
2020-12-07 16:13:12 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'; 
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import './App.css';
+
+class App extends React.Component {  
+  render () {
+    return (
+      <div className='App'>    
+        <Navbar  />
+        <UserItem />
+      </div>
+    );
+  }  
+}
+
+export default App;
