ce8d93ba612a1b1a758007463ac3bbd359fc8ce7
Ian VanDooren <63620074+Ian-VanDooren@users.noreply.github.com>
2021-01-15 16:09:15 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'; 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import './App.css';
 import axios from 'axios';
+import './App.css';
+
 
 class App extends Component {  
   state = {
     users: [],
     loading: false
-  }
+  };
 
  async componenetDidMount () {
     this.setState({ loading: true});
 
-    const res = await axios.get('https://api.github.com/users');
-
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
     this.setState({ users: res.data, loading: false});     
   }
 
